edb7c662428b15bc455081126ba2e91dd8532b16
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-04-24 14:58:59 -0700

    fill in recipe ejs

--- controllers/recipeCtrl.js
+++ controllers/recipeCtrl.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Eecipe= require('../models/recipe')
+const recipe= require('../models/recipe')
 const User = require("../models/user");
 
 // router.get('/recipe',async(req,res)=>{
