bd0fda8a095ef98ffddc0997fd99510380e4951a
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-04-23 19:20:08 -0700

    update server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const express        = require('express')
+const app            = express()
+const bodyParser     = require('body-parser');
+const methodOverride = require('method-override');
+require('./db/db')
+
+
+///controllers
+const cookbookController = require('./controllers/cookbookCtrl')
+const peopleController   = require('./controllers/peopleCtrl')
+const potluckController  = require('./controllers/potluckCtrl')
+const recipeController   = require('./controllers/recipeCtrl')
+const UserController     = require('./controllers/userCtrl')
+
+
+
+
+
+
+///////////middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+
+
+
+
+
+///connection to schema
+
+
+app.listen(3030,()=>{
+    console.log('listening',3030)
+})
