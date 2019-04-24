0a8e4f240d7d847180defd81d27ecc0e5a976367
reactionaryzebra <46640382+reactionaryzebra@users.noreply.github.com>
2019-04-24 09:03:28 -0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
-
-
 const userSchema = new mongoose.Schema({
     username:String,
     password:String,
-    cookbook: [recipies],
-    friends:[users],
-    diets:[str],
-    potLuckOwned:[],
-    potLuckPart:[]
+    cookbook: [{type: mongoose.Schema.Types.ObjectId, ref:'Recipe'}],
+    friends:[{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    diets:[String],
+    potLuckOwned:[{type: mongoose.Schema.Types.ObjectId, ref: 'PotLuck'}],
+    potLuckPart:[{type: mongoose.Schema.Types.ObjectId, ref: 'PotLuck'}]
 })
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
