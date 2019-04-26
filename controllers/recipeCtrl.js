c908f662e24358a50297fde269635e35083898c0
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-04-26 13:42:07 -0700

    wonky test

--- controllers/recipeCtrl.js
+++ controllers/recipeCtrl.js
@@ -46,7 +46,7 @@ router.get("/", async (req, res) => {
     recipes = await axios.get(
       `https://api.edamam.com/search?q=${searchQuery}&app_id=${
         process.env.APP_ID
-      }&app_key=${process.env.APP_KEY}&`
+      }&app_key=${process.env.APP_KEY}&to=100`
     );
     const user = User.findOne({ username: req.session.username });
     res.render("recipes/index", { user, recipes });
